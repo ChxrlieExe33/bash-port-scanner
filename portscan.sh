52f677cdd36be072ad86452d8b8c5920551613d7
Charles <charliedavidcrane13@gmail.com>
2022-07-05 23:45:58 +0200

    First commit

---
+++ portscan.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+declare -a puertosAbiertos=()
+
+function scan (){
+    
+    for ((i=1; i < 10001; i++))
+    do
+
+    if 2> /dev/null > /dev/tcp/$1/$i
+    then
+        puertosAbiertos+=("$i")
+        imprimirPantalla $i
+    fi
+
+    done
+
+}
+
+function imprimirPantalla (){
+
+    cyan='\033[0;36m'
+    green='\033[0;32m'
+    yellow='\033[0;33m'
+    clear='\033[0m'
+
+    echo -e "${yellow}[*] ${green} Port ${cyan} $1 ${green} OPEN ${clear}"
+
+}
+
+function guardarCsv (){
+    for puerto in "${puertosAbiertos[@]}"
+    do
+        echo "$activo;$puerto;abierto" >> resultados.csv
+    done
+}
+
+echo -e "Welcome to my quick bash port scanner \n\nInsert the IP address or domain name you wish to scan: \n"
+
+read -p "--> " activo
+
+if ping -c 1 $activo > /dev/null
+then
+
+    echo -e "\n[+] Scan of $activo running:\n"
+
+    echo -e "---------------------------------------------\n"
+
+    scan $activo
+
+    # imprimirPantalla 
+
+    guardarCsv
+else
+    echo "The target $activo is unavailable or doest exist"
+fi
